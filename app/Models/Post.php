3fe7bd21b3944b1189f1810f935d837f7604b61d
Hikaru-105 <kuri.bou2466@gmail.com>
2023-12-09 11:28:01 +0900

    9-2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,18 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
     
     public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
     
     protected $fillable = [
             'title',
             'body',
+            'category_id',
         ];
 }
